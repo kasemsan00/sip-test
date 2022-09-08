f4d935e0c26150e43dca2c2caabd32e3fa7b71b1
kasemsan00 <kasemsan.cho@gmail.com>
2022-09-08 21:46:04 +0700

    .

--- src/components/Jssip.js
+++ src/components/Jssip.js
@@ -196,13 +196,13 @@ export default function SipJS() {
                     <progress className="progress w-full" value={100} ref={statusBarRef} />
                 </div>
                 <div className="flex m-4 flex-col w-full max-w-full h-[calc(100%-30px)]">
-                    <div className="flex flex-1 h-1/2 w-full drop-shadow-xl self-center items-center justify-center mb-2">
-                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn">
+                    <div className="flex flex-1 h-1/2 w-full self-center items-center justify-center pb-2">
+                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn shadow-lg drop-shadow-lg">
                             <video autoPlay playsInline ref={localVideoRef} className="w-full h-full rounded-2xl hidden" muted></video>
                         </div>
                     </div>
-                    <div className="flex flex-1 h-1/2 w-full drop-shadow-xl self-center items-center justify-center">
-                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn">
+                    <div className="flex flex-1 h-1/2 w-full self-center items-center justify-center pt-2">
+                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn shadow-lg drop-shadow-lg">
                             <video autoPlay playsInline ref={remoteVideoRef} className="w-full h-full rounded-2xl hidden"></video>
                         </div>
                     </div>

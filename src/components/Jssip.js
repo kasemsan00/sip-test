99becf71d706124f843de48325fa18b8a3c3f5a6
kasemsan00 <kasemsan.cho@gmail.com>
2022-09-08 21:47:37 +0700

    .

--- src/components/Jssip.js
+++ src/components/Jssip.js
@@ -197,13 +197,24 @@ export default function SipJS() {
                 </div>
                 <div className="flex m-4 flex-col w-full max-w-full h-[calc(100%-30px)]">
                     <div className="flex flex-1 h-1/2 w-full self-center items-center justify-center pb-2">
-                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn shadow-lg drop-shadow-lg">
-                            <video autoPlay playsInline ref={localVideoRef} className="w-full h-full rounded-2xl hidden" muted></video>
+                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn ">
+                            <video
+                                autoPlay
+                                playsInline
+                                ref={localVideoRef}
+                                className="w-full h-full rounded-2xl hidden shadow-lg drop-shadow-lg"
+                                muted
+                            ></video>
                         </div>
                     </div>
                     <div className="flex flex-1 h-1/2 w-full self-center items-center justify-center pt-2">
-                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn shadow-lg drop-shadow-lg">
-                            <video autoPlay playsInline ref={remoteVideoRef} className="w-full h-full rounded-2xl hidden"></video>
+                        <div className="flex h-full items-center justify-center rounded-2xl animate__animated animate__zoomIn ">
+                            <video
+                                autoPlay
+                                playsInline
+                                ref={remoteVideoRef}
+                                className="w-full h-full rounded-2xl hidden shadow-lg drop-shadow-lg"
+                            ></video>
                         </div>
                     </div>
                 </div>

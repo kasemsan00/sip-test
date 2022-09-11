5591874090c6e0a11343f4198df80ff31f4789db
Kasemsan Chompuwised <kasemsan.cho@gmail.com>
2022-09-11 18:22:40 +0700

    add localview

--- src/components/ViewLocal.js
+++ src/components/ViewLocal.js
@@ -1,3 +1,22 @@
+import { useEffect, useRef } from "react";
+
 export default function ViewLocal() {
-    return <div>LocalVideo</div>;
+    const localVideoRef = useRef(null);
+
+    useEffect(() => {
+        initialLocalCamera();
+    }, []);
+
+    const initialLocalCamera = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+            console.log("stream", stream);
+            localVideoRef.current.srcObject = stream;
+        });
+    };
+
+    return (
+        <div className="pt-2">
+            <video className="w-full h-[150px] rounded-xl bg-slate-800" autoPlay playsInline ref={localVideoRef}></video>
+        </div>
+    );
 }

9f247c3c74ce86b74819757cdc69039fef83a049
kasemsan00 <kasemsan.cho@gmail.com>
2022-09-18 13:08:17 +0700

    init

--- vercel.sh
+++ vercel.sh
@@ -1,2 +1,6 @@
-vercel build
-vercel --prod
+VERCEL_TOKEN=2KNREOkWWK6rbs3nIzKWKJlA
+# vercel เข้าระบบก่อน
+# npm install -g vercel 
+vercel pull --yes --environment=production --token=$VERCEL_TOKEN
+vercel build --prod --token=$VERCEL_TOKEN
+vercel deploy --prebuilt --prod --token=$VERCEL_TOKEN
